d8d4ba65bc82d1e002a0e0da65a75c7619ee71a0
Nikolay <yakolya19@gmail.com>
2024-06-14 16:18:12 +0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Введите имя: ";
+	string name;
+	cin >> name;
+	cout << "Здравствуйте, " << name;
+	return 0;
+}
